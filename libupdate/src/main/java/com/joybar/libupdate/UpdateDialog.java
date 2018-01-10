15df9466f99a991a382ff398aead3a3b3cbb1c25
joybar <myjoybar@gmail.com>
2018-01-10 18:18:44 +0800

    Update dialog opt

--- libupdate/src/main/java/com/joybar/libupdate/UpdateDialog.java
+++ libupdate/src/main/java/com/joybar/libupdate/UpdateDialog.java
@@ -2,9 +2,14 @@ package com.joybar.libupdate;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.joybar.libupdate.iml.IConfirmDialog;
@@ -19,20 +24,20 @@ public class UpdateDialog extends Dialog implements android.view.View.OnClickLis
     private int layoutRes;
     private Context context;
     private boolean mIsForceUpdate;
-    private String content;
     private String title;
-
+    private  List<String> contentList;
     public Button confirmBtn;
     private Button cancelBtn;
     TextView txt_title;
     TextView txt_tips;
+    LinearLayout lineLayoutContent;
 
     private String getListStr(List<String> contentList){
         if(null != contentList || contentList.size()!=0){
             int count = contentList.size();
             StringBuilder sb = new StringBuilder();
             for(int i = 0;i<count;i++){
-                sb.append((i+1)+contentList.get(i)+"/n");
+                sb.append((i+1)+". "+contentList.get(i)+"\n");
             }
             return sb.toString();
         }
@@ -45,7 +50,7 @@ public class UpdateDialog extends Dialog implements android.view.View.OnClickLis
         this.context = context;
         this.layoutRes = R.layout.custom_confirmdialog;
         this.title = title;
-        this.content = getListStr(contentList);
+        this.contentList = contentList;
         mIsForceUpdate = isForceUpdate;
     }
 
@@ -82,8 +87,24 @@ public class UpdateDialog extends Dialog implements android.view.View.OnClickLis
         txt_title.setText(title);
 
         // 显示提示文本
-        txt_tips = (TextView) findViewById(R.id.txt_content);
-        txt_tips.setText(content);
+        lineLayoutContent = findViewById(R.id.ll_content);
+        if(null != contentList || contentList.size()!=0) {
+            int count = contentList.size();
+            for(int i = 0;i<count;i++){
+                TextView showText = new TextView(context);
+                showText.setTextColor(Color.parseColor("#333333"));
+                showText.setTextSize(TypedValue.COMPLEX_UNIT_SP,14);
+                showText.setPadding(dp2px(context,8),dp2px(context,2),dp2px(context,8),dp2px(context,2));
+                showText.setText((i+1)+". "+contentList.get(i));
+                lineLayoutContent.addView(showText);
+            }
+            if(count>=12){
+              LinearLayout lineLayoutContentParent = findViewById(R.id.ll_content_parent);
+                LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) lineLayoutContentParent.getLayoutParams();
+                layoutParams.height = getScreenHeight(context)*3/7;
+                lineLayoutContentParent.setLayoutParams(layoutParams);
+            }
+        }
 
         // 为按钮绑定点击事件监听器
         confirmBtn.setOnClickListener(this);
@@ -105,5 +126,22 @@ public class UpdateDialog extends Dialog implements android.view.View.OnClickLis
         }
 
     }
+
+
+    public static int dp2px(Context context,float dip) {
+        float scale = context.getResources()
+                .getDisplayMetrics().density;
+        return (int) (dip * scale + 0.5f * (dip >= 0 ? 1 : -1));
+    }
+
+
+    public static int getScreenHeight(Context context) {
+        WindowManager wm = (WindowManager) context
+                .getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(outMetrics);
+        return outMetrics.heightPixels;
+    }
+
 
 }
